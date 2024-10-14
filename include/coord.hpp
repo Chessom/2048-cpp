a2bdd4f8161ff8b84afd1c6ec19cb26647bd5acf
Vemy <68952024+Chessom@users.noreply.github.com>
2024-10-14 15:59:56 +0800

    实现了2048的核心

---
+++ include/coord.hpp
@@ -0,0 +1,45 @@
+#pragma once
+#include <random>
+#include <vector>
+namespace core {
+namespace direction {
+    enum {
+        left = 0,
+        down,
+        right,
+        up
+    };
+} // namespace direction
+template <typename T>
+struct coord_t {
+    coord_t()
+        = default;
+    coord_t(T x, T y)
+        : x(x)
+        , y(y)
+    {
+    }
+    coord_t(const std::pair<int, int>& p)
+        : x(p.first)
+        , y(p.second) { };
+    void shift(int dir)
+    {
+        switch (dir) {
+        case direction::up:
+            y--;
+            break;
+        case direction::down:
+            y++;
+            break;
+        case direction::left:
+            x--;
+            break;
+        case direction::right:
+            x++;
+            break;
+        }
+    }
+    T x = 0, y = 0;
+};
+using coord = coord_t<int>;
+}
